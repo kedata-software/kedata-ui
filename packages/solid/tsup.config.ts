a3cee4a0dfe1a09d320874ebf76b109fbf63ecf9
Alfian Andi Nugraha <me@alfianandi.dev>
2025-04-09 14:28:11 +0700

    fix(solid): fix dts and type system

--- packages/solid/tsup.config.ts
+++ packages/solid/tsup.config.ts
@@ -1,14 +1,24 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: {
+    index: 'src/index.ts',
+  },
   format: ['esm'],
-  dts: true,
+  dts: {
+    compilerOptions: {
+      paths: {
+        '@kedata-ui/slots': ['packages/slots/src/index.ts'],
+        '@kedata-ui/slots/*': ['packages/slots/src/*'],
+        '@kedata-ui/solid-icons': ['packages/solid-icons/src/index.ts'],
+      },
+    },
+  },
   minify: false,
   clean: true,
   splitting: true,
   sourcemap: true,
-  external: ['hex-rgb', 'clsx', '@zag-js'],
+  external: ['hex-rgb', 'clsx', '@zag-js', '@kedata-software'],
   treeshake: false,
   target: 'es2021',
   tsconfig: './tsconfig.build.json',
