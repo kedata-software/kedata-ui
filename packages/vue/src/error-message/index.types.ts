adf5b6e462c8a47dfa7ae239c5a6ebf68babbfd8
Alfian Andi Nugraha <me@alfianandi.dev>
2025-05-30 20:42:52 +0700

    feat(vue): support colorPalette in ErrorMessage

--- packages/vue/src/error-message/index.types.ts
+++ packages/vue/src/error-message/index.types.ts
@@ -1,4 +1,7 @@
+import type { ColorPalette } from '@kedata-ui/slots';
+
 export type ErrorMessageProps = {
+  colorPalette?: ColorPalette;
   withParts?: boolean;
   class?: string;
   for?: string;
--- packages/vue/src/error-message/useErrorMessage.ts
+++ packages/vue/src/error-message/useErrorMessage.ts
@@ -1,8 +1,11 @@
-import { errorMessageSlots, tw } from "@kedata-ui/slots";
-import { computed, type HTMLAttributes } from "vue";
-import type { ErrorMessageProps } from "./index.types";
+import { errorMessageSlots, tw } from '@kedata-ui/slots';
+import { computed, type HTMLAttributes } from 'vue';
+import type { ErrorMessageProps } from './index.types';
+import { useColorPalette } from '../use-color-palette';
 
 const useErrorMessage = (props: ErrorMessageProps) => {
+  const colorPaletteClass = useColorPalette(() => props.colorPalette, 'danger');
+
   const slots = computed(() => {
     return errorMessageSlots({
       withParts: props.withParts,
@@ -11,7 +14,7 @@ const useErrorMessage = (props: ErrorMessageProps) => {
 
   const getRootProps = () => {
     return {
-      class: tw("color-palette-danger", slots.value.root(), props.class),
+      class: tw(colorPaletteClass.value, slots.value.root(), props.class),
     } as HTMLAttributes;
   };
 
