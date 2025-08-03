35be24211ce40ac2cf0293f00b7659b0ff324cbf
Alfian Andi Nugraha <me@alfianandi.dev>
2025-08-03 19:30:32 +0700

    fix(vue): unify quotation style in dialog exports for consistency across the module

--- packages/vue/src/dialog/index.ts
+++ packages/vue/src/dialog/index.ts
@@ -1,13 +1,13 @@
-export { default as DialogContent } from "./DialogContent.vue";
+export { default as DialogContent } from './DialogContent.vue';
 
-export { default as DialogHeader } from "./DialogHeader.vue";
+export { default as DialogHeader } from './DialogHeader.vue';
 
-export { default as DialogBody } from "./DialogBody.vue";
+export { default as DialogBody } from './DialogBody.vue';
 
-export { default as DialogFooter } from "./DialogFooter.vue";
+export { default as DialogFooter } from './DialogFooter.vue';
 
-export { default as withDialog } from "./withDialog";
+export { default as withDialog } from './withDialog';
 
-export { default as useDialogStore } from "./useDialogStore";
+export { default as useDialogStore } from './useDialogStore';
 
-export type * from "./index.types";
+export type * from './index.types';
