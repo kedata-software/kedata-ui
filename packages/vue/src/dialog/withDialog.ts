83655c162d03acac75b046ca445ed123d17faf5a
Alfian Andi Nugraha <me@alfianandi.dev>
2025-08-03 19:30:48 +0700

    fix(vue): unify quotation style in withDialog component for consistency across the dialog module

--- packages/vue/src/dialog/withDialog.ts
+++ packages/vue/src/dialog/withDialog.ts
@@ -1,7 +1,7 @@
-import { DialogContextKey } from "./DialogContext";
-import animateStatePreset from "../animate-state-preset";
-import * as dialog from "@zag-js/dialog";
-import { normalizeProps, useMachine } from "@zag-js/vue";
+import { DialogContextKey } from './DialogContext';
+import animateStatePreset from '../animate-state-preset';
+import * as dialog from '@zag-js/dialog';
+import { normalizeProps, useMachine } from '@zag-js/vue';
 import {
   computed,
   defineComponent,
@@ -11,12 +11,12 @@ import {
   Transition,
   useId,
   type HTMLAttributes,
-} from "vue";
-import type { WithDialogProps } from "./index.types";
+} from 'vue';
+import type { WithDialogProps } from './index.types';
 
 const withDialog = (DialogComponent: any) => {
   return defineComponent({
-    name: "WithDialog",
+    name: 'WithDialog',
     props: {
       store: {
         type: Object,
@@ -24,7 +24,7 @@ const withDialog = (DialogComponent: any) => {
       },
       position: {
         type: String,
-        default: "center",
+        default: 'center',
       },
       paddingless: {
         type: Boolean,
@@ -45,14 +45,14 @@ const withDialog = (DialogComponent: any) => {
             restoreFocus: true,
             closeOnEscape: true,
             modal: true,
-            role: "dialog" as const,
+            role: 'dialog' as const,
             open: store.isOpen.value,
             onOpenChange: ({ open }) => {
               if (open) store.open();
               else store.close();
             },
           };
-        })
+        }),
       );
 
       const api = computed(() => dialog.connect(service, normalizeProps));
@@ -60,7 +60,8 @@ const withDialog = (DialogComponent: any) => {
       const getBackdropProps = () => {
         return {
           ...api.value.getBackdropProps(),
-          class: "bg-black/50 absolute inset-0 transition-opacity",
+          hidden: undefined,
+          class: 'bg-black/50 absolute inset-0 opacity-0',
         } as HTMLAttributes;
       };
 
@@ -68,9 +69,9 @@ const withDialog = (DialogComponent: any) => {
         return {
           ...api.value.getPositionerProps(),
           class:
-            "fixed w-full top-0 left-1/2 h-full overflow-hidden -translate-x-1/2 transition-[opacity,top]",
+            'fixed w-full top-0 left-1/2 h-full overflow-hidden -translate-x-1/2 transition-[opacity,top]',
           style: {
-            "--tw-translate-x": "-50.1%",
+            '--tw-translate-x': '-50.1%',
           },
         } as HTMLAttributes;
       };
@@ -81,9 +82,9 @@ const withDialog = (DialogComponent: any) => {
           props.store.close();
 
           setTimeout(() => {
-            document.body.style.overflow = "";
-            document.body.style.paddingRight = "";
-            document.body.removeAttribute("data-scroll-lock");
+            document.body.style.overflow = '';
+            document.body.style.paddingRight = '';
+            document.body.removeAttribute('data-scroll-lock');
           }, 0);
         },
         position: props.position,
@@ -93,43 +94,41 @@ const withDialog = (DialogComponent: any) => {
       return () => {
         return h(
           Teleport,
-          { to: "body" },
-          h("div", {}, [
+          { to: 'body' },
+          h('div', {}, [
             h(
               Transition,
               {
                 duration: 150,
                 class: animateStatePreset.fade.base,
-                enterFromClass: animateStatePreset.fade["enter-from"],
-                enterActiveClass: animateStatePreset.fade["enter-active"],
-                enterToClass: animateStatePreset.fade["enter-to"],
-                leaveFromClass: animateStatePreset.fade["leave-from"],
-                leaveActiveClass: animateStatePreset.fade["leave-active"],
-                leaveToClass: animateStatePreset.fade["leave-to"],
+                enterFromClass: animateStatePreset.fade['enter-from'],
+                enterToClass: animateStatePreset.fade['enter-to'],
+                leaveFromClass: animateStatePreset.fade['leave-from'],
+                leaveActiveClass: animateStatePreset.fade['leave-active'],
+                leaveToClass: animateStatePreset.fade['leave-to'],
               },
               () => {
-                return isOpen.value ? h("div", getBackdropProps()) : null;
-              }
+                return isOpen.value ? h('div', getBackdropProps(), []) : null;
+              },
             ),
             h(
               Transition,
               {
                 duration: 150,
                 class: animateStatePreset.fadeUp.base,
-                enterFromClass: animateStatePreset.fadeUp["enter-from"],
-                enterActiveClass: animateStatePreset.fadeUp["enter-active"],
-                enterToClass: animateStatePreset.fadeUp["enter-to"],
-                leaveFromClass: animateStatePreset.fadeUp["leave-from"],
-                leaveActiveClass: animateStatePreset.fadeUp["leave-active"],
-                leaveToClass: animateStatePreset.fadeUp["leave-to"],
+                enterFromClass: animateStatePreset.fadeUp['enter-from'],
+                enterToClass: animateStatePreset.fadeUp['enter-to'],
+                leaveFromClass: animateStatePreset.fadeUp['leave-from'],
+                leaveActiveClass: animateStatePreset.fadeUp['leave-active'],
+                leaveToClass: animateStatePreset.fadeUp['leave-to'],
               },
               () => {
                 return isOpen.value
-                  ? h("div", getPositionerProps(), [h(DialogComponent)])
+                  ? h('div', getPositionerProps(), [h(DialogComponent)])
                   : null;
-              }
+              },
             ),
-          ])
+          ]),
         );
       };
     },
